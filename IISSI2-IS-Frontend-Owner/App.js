469672af28bc6d38354e13da92ed821d9610d1b6
alevelmol <alevelmol@alum.us.es>
2024-03-22 18:57:54 +0100

    haced el modulo 5 y el styling

---
+++ IISSI2-IS-Frontend-Owner/App.js
@@ -0,0 +1,27 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import * as React from 'react'
+import ControlPanelScreen from './src/screens/controlPanel/ControlPanelScreen'
+import ProfileScreen from './src/screens/profile/ProfileScreen'
+// import RestaurantsScreen from './src/screens/restaurants/RestaurantsScreen'
+import RestaurantsStack from './src/screens/restaurants/RestaurantsStack'
+
+const Tab = createBottomTabNavigator()
+
+export default function App () {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Restaurants"
+          component={RestaurantsStack} />
+        <Tab.Screen
+          name="Control Panel"
+          component={ControlPanelScreen} />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
